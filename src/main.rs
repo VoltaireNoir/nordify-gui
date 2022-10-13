5c25b65bd3b8957ca1e2da296150e346f336615f
VoltaireNoir <mzahmed95@gmail.com>
2022-10-13 18:22:49 +0530

    Add support for basic keyboard shortcuts

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use iced::{Length,Settings,Color,keyboard, executor};
+use iced::{Length,Settings,Color,executor};
 use iced::pure::{Element,column,row, Application};
 
 mod menu;
@@ -20,12 +20,14 @@ struct NordifyGUI {
     previews: Previews,
     browser: Browser,
     menu: Menu,
+    exit: bool,
 }
 
 #[derive(Clone, Debug)]
 pub enum Event {
     Browser(BrowserEvent),
     Menu(MenuEvent),
+    Quit,
 }
 
 impl Application for NordifyGUI {
@@ -45,6 +47,7 @@ impl Application for NordifyGUI {
         match message {
             Event::Browser(event) => self.browser.update(&mut self.previews, &mut self.menu, event),
             Event::Menu(event) => self.menu.update(&mut self.previews, &mut self.browser, event),
+            Event::Quit => self.exit = true,
         };
 
         iced::Command::none()
@@ -74,5 +77,30 @@ impl Application for NordifyGUI {
 
     fn background_color(&self) -> Color {
         theme::BLUE
+    }
+
+    fn subscription(&self) -> iced::Subscription<Self::Message> {
+        iced_native::subscription::events_with(|e,_| {
+
+            use iced_native::keyboard::{self, KeyCode, Modifiers};
+            use iced_native::Event as KeyEvent;
+
+            match e {
+                KeyEvent::Keyboard(keyboard::Event::KeyPressed { key_code, modifiers }) => {
+                    match (key_code, modifiers) {
+                        (KeyCode::P, Modifiers::CTRL) => Some(Event::Menu(MenuEvent::Preview)),
+                        (KeyCode::R, Modifiers::CTRL) => Some(Event::Menu(MenuEvent::Reset)),
+                        (KeyCode::S, Modifiers::CTRL) => Some(Event::Menu(MenuEvent::Save)),
+                        (KeyCode::Q, Modifiers::CTRL) => Some(Event::Quit),
+                        _ => None,
+                    }
+                },
+                _ => None,
+            }
+        })
+    }
+
+    fn should_exit(&self) -> bool {
+        self.exit
     }
 }
